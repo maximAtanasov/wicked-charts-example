4dd29776f55e3cfce4270a3cbd33f81a22d8c1d3
maximAtanasov <Maksim.Atanasov@adesso.de>
2018-06-27 13:59:16 +0200

    fixed missing dependency

---
+++ README.md
@@ -0,0 +1,4 @@
+A minimal example, showing a line chart in a Wicket 7 web app.
+
+To start the application run `./gradlew bootRun` in the root folder
+of the project and then go to `localhost:8080` in you browser.
--- src/main/java/de/adesso/wickedcharts/example/Homepage.java
+++ src/main/java/de/adesso/wickedcharts/example/Homepage.java
@@ -6,7 +6,7 @@ import de.adesso.wickedcharts.chartjs.chartoptions.colors.RgbaColor;
 import de.adesso.wickedcharts.chartjs.chartoptions.colors.SimpleColor;
 import de.adesso.wickedcharts.chartjs.chartoptions.label.TextLabel;
 import de.adesso.wickedcharts.chartjs.chartoptions.valueType.IntegerValue;
-import de.adesso.wickedcharts.wicket8.chartjs.Chart;
+import de.adesso.wickedcharts.wicket7.chartjs.Chart;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
@@ -25,10 +25,10 @@ public class Homepage extends WebPage implements Serializable {
         Data data = new Data().setLabels(TextLabel.of("January", "February", "March", "April", "May", "June", "July"));
 
         Dataset dataset1 = new Dataset()
-                .setLabel("My first dataset")  //Ein Label für den Datensatz
-                .setBackgroundColor(SimpleColor.RED) //Wir wählen eine Farbe aus dem SimpleColor Enum, dieses Enum enthält einige der am häufigsten verwendeten Farben.
-                .setBorderColor(new RgbaColor(0, 50, 255, 255)) //Sie können aber auch Ihre eigene Farbe mit den Klassen RgbaColor/HexColor definieren.
-                        .setData(IntegerValue.of(6,7,8,2,1,1,3)) //Hier setzen wir die eigentlichen Datenpunkte.
+                .setLabel("My first dataset")
+                .setBackgroundColor(SimpleColor.BLUE)
+                .setBorderColor(SimpleColor.BLUE)
+                        .setData(IntegerValue.of(6,7,8,2,1,1,3))
                         .setFill(false);
 
         data.setDatasets(Arrays.asList(dataset1));
@@ -39,24 +39,24 @@ public class Homepage extends WebPage implements Serializable {
                 .setResponsive(true)
                 .setTitle(new Title()
                         .setDisplay(true)
-                        .setText("Chart.js Line Chart")) //Der Titel unseres Diagramms.
-                .setTooltips(new Tooltips() //Unsere Tooltips (Diese sieht man beim Überfahren eines Datenpunktes)
+                        .setText("Chart.js Line Chart"))
+                .setTooltips(new Tooltips()
                         .setMode(TooltipMode.INDEX)
                         .setIntersect(false))
                 .setHover(new Hover()
                         .setMode(HoverMode.NEAREST)
                         .setIntersect(true))
                 .setScales(new Scales()
-                        .setXAxes(new AxesScale() //Wir konfigurieren die X-Achse
+                        .setXAxes(new AxesScale()
                                 .setDisplay(true)
                                 .setScaleLabel(new ScaleLabel()
                                         .setDisplay(true)
                                         .setLabelString("Month")))
-                        .setYAxes(new AxesScale() //Und die Y-Achse
-                                .setDisplay(true)
+                        .setYAxes(new AxesScale()
                                 .setTicks(new Ticks()
                                         .setMax(10)
                                         .setMin(0))
+                                .setDisplay(true)
                                 .setScaleLabel(new ScaleLabel()
                                         .setDisplay(true)
                                         .setLabelString("Value"))));
